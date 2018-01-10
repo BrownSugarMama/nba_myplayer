b8f761f0cd21276729cc6fe2762ccfdab39c5ad0
neechie <neechieg@gmail.com>
2018-01-10 11:30:03 -0500

    trying to debug

--- controllers/myplayers.js
+++ controllers/myplayers.js
@@ -2,12 +2,10 @@ const express  = require('express')
 const router   = express.Router()
 
 router.get('/', (req, res) => {
-	res.render('layout.hbs')
-}
+  res.render('welcome')
+})
 
 module.exports = router
-
-
 
 // const express   = ('express')
 // const myPlayer  = require('../db/models/myPlayer')
--- index.js
+++ index.js
@@ -2,25 +2,25 @@ const express       = require ('express')
 const parser        = require ('body-parser')
 const hbs           = require ('express-handlebars')
 // const methodOveride = require ('method-override')
-const MyPlayer      = require ('./controllers/myPlayers')
+const MyPlayers     = require ('./controllers/myPlayers')
 const app           = express ()
 
 app.set('port', process.env.PORT || 4001)
 app.set('view engine', 'hbs')
 
 app.engine('.hbs', hbs({
-      extname:        '.hbs',
-      partialsDir:    'views/',
-      layoutsDir:     'views/',
-      defaultLayout:  'layout'
-  })
+  extname:        '.hbs',
+  partialsDir:    'views/',
+  layoutsDir:     'views/',
+  defaultLayout:  'layout'
+})
 );
 
 app.use('/assets', express.static('public'))
 
 app.use(parser.urlencoded({ extended: true }))
 
-app.use('/myPlayers', myPlayers)
+app.use('/', MyPlayers)
 
 // app.use(methodOverride('_method'))
 
