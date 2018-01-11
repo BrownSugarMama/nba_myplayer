c71339c587292dca2432ecaf5dd997b24638639f
neechie <neechieg@gmail.com>
2018-01-11 06:49:18 -0500

    cleaned up code. Added more text to views.

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 const express         = require ('express')
 const parser          = require ('body-parser')
 const hbs             = require ('express-handlebars')
-// const methodOverride  = require ('method-override')
-const MyPlayers       = require ('./controllers/myPlayers')
+const methodOverride  = require ('method-override')
+const myPlayers       = require ('./controllers/myPlayers')
 const app             = express ()
 
 app.set('port', process.env.PORT || 3050)
-
-app.listen(app.get('port'), () => {
-  console.log(`WHAT UP DOE ${app.get('port')} AYEEEEEE 🌟`)
-})
 
 app.set('view engine', 'hbs')
 app.engine('.hbs', hbs({
@@ -24,10 +20,14 @@ app.use('/assets', express.static('public'))
 
 app.use(parser.urlencoded({ extended: true }))
 
-app.use('/', MyPlayers)
+app.use('/myPlayers', myPlayers)
 
-// app.use(methodOverride('_method'))
+app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
   res.render('welcome')
+})
+
+app.listen(app.get('port'), () => {
+  console.log(`WHAT UP DOE ${app.get('port')} AYEEEEEE 🌟`)
 })
