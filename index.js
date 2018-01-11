4c8344fa1cacfa755fd08d0a2fcc0eaa36bbf5fe
neechie <neechieg@gmail.com>
2018-01-11 04:47:38 -0500

    Debugging worked. Able to console log local host again.

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-const express       = require ('express')
-const parser        = require ('body-parser')
-const hbs           = require ('express-handlebars')
-// const methodOveride = require ('method-override')
-const MyPlayers     = require ('./controllers/myPlayers')
-const app           = express ()
+const express         = require ('express')
+const parser          = require ('body-parser')
+const hbs             = require ('express-handlebars')
+const methodOverride  = require ('method-override')
+const MyPlayers       = require ('./controllers/myPlayers')
+const app             = express ()
 
-app.set('port', process.env.PORT || 4001)
+app.set('port', process.env.PORT || 3050)
+
+app.listen(app.get('port'), () => {
+  console.log(`WHAT UP DOE ${app.get('port')} AYEEEEEE 🌟`)
+})
+
 app.set('view engine', 'hbs')
-
 app.engine('.hbs', hbs({
   extname:        '.hbs',
   partialsDir:    'views/',
@@ -22,15 +26,10 @@ app.use(parser.urlencoded({ extended: true }))
 
 app.use('/', MyPlayers)
 
-// app.use(methodOverride('_method'))
+app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
   res.render('welcome')
-})
-app.set('port', process.env.PORT || 4001)
-
-app.listen(app.get('port'), () => {
-  console.log(`WHAT UP DOE ${app.get('port')} 🌟`)
 })
 
 // app.get('/', (req, res) => {
