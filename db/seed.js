c42d7a76051e67b1785519193ced77e5b0d830f9
neechie <neechieg@gmail.com>
2018-01-11 10:20:58 -0500

    added information to database

--- db/seed.js
+++ db/seed.js
@@ -2,7 +2,7 @@ const mongoose  = require('./connection')
 const seedData    = require('./playerData.json')
 const myPlayer = mongoose.model('myPlayer');
 
-// Not sure which way this variable should be declared
+// Not sure which way or even if this variable should be declared
 // const myPlayer = mongoose.model('myPlayer', MyPlayerSchema)
 
 mongoose.Promise = Promise
@@ -12,5 +12,5 @@ myPlayer.remove({}).then(_ => {
  myPlayer.collection.insert(seedData).then(seededEntries => {
   console.log(seededEntries)
   mongoose.connection.close()
- })
+  })
 })
