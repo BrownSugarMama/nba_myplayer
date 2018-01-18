67b5849377ac4ea43a40406ecf3b67da76c40cc5
neechie <neechieg@gmail.com>
2018-01-18 14:14:18 -0500

    making sure local updates are pushed

--- controllers/myplayers.js
+++ controllers/myplayers.js
@@ -12,12 +12,13 @@ const MyPlayer = mongoose.model('MyPlayer')
 // Get the list/info of MyPlayers
 router.get('/', (req, res) => {
   MyPlayer.find({})
-    .then((myplayers) => {
+    .then((myplayers => {
       res.render('myPlayerList', {
         myplayers: myplayers
       })
     })
 })
+
 
 // Get or find a single of MyPlayers
 router.get('/:position', (req, res) => {
@@ -28,39 +29,39 @@ router.get('/:position', (req, res) => {
   })
 })
 
+// Add a myPlayer
+router.post('/:position', (req, res) => {
+  Recipe.create(req.body.position).then(myPlayer => {
+      res.redirect(`/myplayer/${myplayer.name}`);
+  });
+});
+
 // Update and edit a MyPlayer
-// router.get('/', (req, res) => {
-//   res.render('edit')
-//   })
-
-router.get('/myPlayer', (req, res) => {
-  res.render('edit')
-})
-
 router.put('/:name', (req, res) => {
   MyPlayer.findOneAndUpdate({ name: req.params.name }, req.body.myPlayer, { new: true })
   .then(myPlayer => {
-    res.redirect(`/myPlayer/${myPlayer.name}`)
+    res.redirect(`/myPlayer/${myPlayer.position}`)
   })
 })
 
-// Add a new Myplayer's info
-router.post('/', (req, res) => {
-  MyPlayer.create(req.body.myplayer)
-    .then(myplayer => {
-      res.redirect(`/myPlayers`)
-    })
-})
 
+// Add a new Myplayer's info
+
+router.post('/', (req, res) => {
+    Recipe.create(req.body.myplayer).then(myplayer => {
+        res.redirect(`/myPlayers/${myplayer.name}`);
+    });
+});
 
 //Delete a MyPlayer. Not sure if I want this functionality on my app
 router.delete('/:name', (req, res) => {
   MyPlayer.findOneAndRemove({ name: req.params.name })
-  .then(myPlayer => {
+  .then(() => {
     res.redirect('/myPlayer')
   })
 })
 
 
-module.exports = router
 
+
+module.exports = router
